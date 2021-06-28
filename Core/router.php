3c6d0e7843949eac28689591448ed613d9713526
Mehovic99 <anesmehovic28@gmail.com>
2021-06-28 13:48:06 +0200

    working on router php

--- Core/router.php
+++ Core/router.php
@@ -15,6 +15,59 @@ class Router
     $route = preg_replace('/\{([a-z]+)\}','(?P<\1>[a-z-]+)', $route);
 
     $route = preg_replace('/\{ ([a-z]+) : ([⌃\}]+)}/','(?P<\1>\2', $route);
+
+    $route = '/⌃' . $route . '$/i';
+
+    this->routes[$route]=$params;
+  }
+
+  public function getRoutes()
+  {
+    return this->routes;
+  }
+
+  public function match($url)
+  {
+    foreach ($this->routes as $route => $params){
+      if(preg_match($route,$url,$matches)){
+        foreach($matches as $key => $match){
+          if(is_string($key)){
+            $params[$key]=$match;
+          }
+        }
+        $this->$patams = $patams;
+        return true;
+      }
+    }
+      return false;
+  }
+  public function getParams()
+  {
+    return $this->params;
+  }
+
+  public function dispatch($url)
+  {
+    $url=$this->removeQueryStringVariables($url)
+
+    if ($this->match($url)){
+      $controller  = $this->params['controller'];
+      $controller = $this->convertToStudlyCaps($controller);
+      $controller = $this->getNamespace() . $controller;
+
+      if (class_exists($controller)){
+        $controller_object = new $controller($this->params);
+
+        $action = $this->params['action'];
+        $action = $this->convertTo
+      }
+    }
+  }
+
+  protected function removeQueryStringVariables($url){
+    if ($url!=''){
+      $parts=explode
+    }
   }
 }
 
